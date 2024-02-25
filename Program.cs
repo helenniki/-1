49649d857234fcb74b2d773c1c994070c85be9b2
Lena Nik <pena_lena99@mail.ru>
2024-02-25 20:24:13 +0300

    Написала метод который подсчитывает кол-во нужных строк, и записывает интересные строки в новый массив

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿// Задача: Написать программу, которая из имеющегося массива строк
+//  формирует новый массив из строк, длина которых меньше, 
+//  либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//  либо задать на старте выполнения алгоритма. 
+//  При решении не рекомендуется пользоваться коллекциями, 
+//  лучше обойтись исключительно массивами.
+
+string[] initialArray = { "of", "orange", "cat", "to", "elephant", "fox", "beer" };
+string[] FilterArrayByLength(string[] array)
+    {
+    
+        int count = 0;
+        foreach (string item in array)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] filteredArray = new string[count];
+
+        int index = 0;
+        foreach (string item in array)
+        {
+            if (item.Length <= 3)
+            {
+                filteredArray[index] = item;
+                index++;
+            }
+        }
+
+        return filteredArray;
+    }
+
